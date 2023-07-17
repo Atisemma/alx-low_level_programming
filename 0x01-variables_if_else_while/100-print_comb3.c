f3f6b9be8592e7c55d3a4fbfd36196824bb8bc86
Atisemma <atisemma26@gmail.com>
2023-07-17 13:49:57 -0700

    Invention

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
+
 /**
-  * main - Prints all possible different combinations of two digits
-  *
-  * Return: Always (0)
-  */
+ * main - Prints all possible combinations of two different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int a, b;
+	int digit1, digit2;
 
-	for (a = 0; a <= 9; a++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
-			if (a < b)
-			{
-				putchar(a);
-				putchar(b);
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
 
-				if (a != '8' || (a == '8' && b != 9))
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,11 +20,11 @@ int main(void)
 					putchar(b);
 					putchar(c);
 
-					if (a != 7)
+					if (a != 7 || (b <= 8 && c <= 9))
 
 					putchar(',');
 					putchar(' ');
-			}
+				}
 
 			}
 		}
