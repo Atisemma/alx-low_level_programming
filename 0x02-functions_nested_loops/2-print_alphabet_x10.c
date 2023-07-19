daa99673e23b2957cecf4e9c6e1b938b4f286af0
Atisemma <atisemma26@gmail.com>
2023-07-19 13:02:37 -0700

    Alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 	int a;
 	int b;
 
-	for (a = 0; a <= 10; a++)
+	for (a >= 0; a <= 10; a++)
 	{
-	for (b = 97; b <= 122; b++)
+	for (b >= 97; b <= 122; b++)
 	{
 		_putchar(b);
 	}
