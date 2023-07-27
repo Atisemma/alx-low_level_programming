d7cdad89eb1f44f2c171821f121133dd6fee2356
Atisemma <atisemma26@gmail.com>
2023-07-27 13:38:04 -0700

    Strings concentrated

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncat - Write a function that concatenates two strings
+ * @dest: A pointer to the destination
+ * @src: A pointer too the source
+ * @n: bytes to be concatenated from src
+ * Return: A pointer to the resulting string dest
+ *
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *temp = dest;
+
+	while (*dest)
+	dest++;
+
+	while (n && *src)
+		*dest++ = *src++;
+	n--;
+
+	*dest = '\0';
+
+	return (temp);
+}
