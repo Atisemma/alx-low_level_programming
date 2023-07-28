179d1ba4c247ee081174c8d1d97ccc93a3cb7e11
Atisemma <atisemma26@gmail.com>
2023-07-28 15:38:30 -0700

    Encoding a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,22 +7,19 @@
 char *leet(char *str)
 {
 	char *ptr = str;
-	char *letters = "aAeEoOtTlL";
-	char *leets = "4433007711";
-	int i = 0;
-	int j;
+	char letters[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char leets[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	int i;
 
 	while (*ptr != '\0')
 	{
-		j = 0;
-		while (letters[j] != '\0')
+		for (i = 0; i < 10; i++)
 		{
-			if (*ptr == letters[j])
+			if (*ptr == letters[i])
 			{
-				*ptr = leets[j];
+				*ptr = leets[i];
 				break;
 			}
-			j++;
 		}
 		ptr++;
 	}
