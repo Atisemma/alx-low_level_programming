d109bd2b2ad3d271592d317ab8bb60b401841d53
Atisemma <atisemma26@gmail.com>
2023-07-27 17:06:21 -0700

    Capitalization

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stddef.h>
+#include <ctype.h>
+/**
+ * cap_string - capitalizes all word of a string
+ * @str: input string
+ *
+ * Return: Pointer to the resultIng string
+ */
+char *cap_string(char *str)
+{
+	if (str != NULL)
+	{
+		int capitalize_next = 1;
+		char *ptr = str;
+
+		while (*ptr != '\0')
+		{
+			if (capitalize_next && islower(*str))
+			{
+				*ptr = toupper(*ptr);
+			}
+		}
+		if (*ptr == ' ' || *ptr == '\t' || *ptr == '\n' || *ptr == ' '
+		|| *ptr == ';' || *ptr == '.' ||
+		*ptr == '!' || *ptr == '?' || *ptr == '"'
+		|| *ptr == '(' || *ptr == ')' || *ptr == '{' || *ptr == '}')
+		{
+			capitalize_next = 1;
+		}
+		else
+		{
+			capitalize_next = 0;
+		}
+		ptr++;
+	}
+
+	return (str);
+}
