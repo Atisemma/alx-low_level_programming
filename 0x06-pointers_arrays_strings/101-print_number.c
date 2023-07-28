a367a7366928277779e8db4c5bac61fc81fc3539
Atisemma <atisemma26@gmail.com>
2023-07-28 16:53:01 -0700

    Print an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,20 +5,18 @@
  */
 void print_number(int n)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
+	unsigned int x;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		n *= -1;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
+	x = n;
 
-	_putchar(n % 10 + '0');
+	if (x / 10)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
 }
