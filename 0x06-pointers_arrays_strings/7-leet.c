dabad175029f89ca0db164dcc01cb024ca85eca1
Atisemma <atisemma26@gmail.com>
2023-07-28 15:09:01 -0700

    Encoding a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * leet - encodes a string
+ * @str: the input string
+ * Return: the encoded string
+ */
+char *leet(char *str)
+{
+	char *ptr = str;
+	char *letters = "aAeEoOtTlL";
+	char *leets = "443370711";
+	int i = 0;
+	int j;
+
+	while (*ptr != '\0')
+	{
+		j = 0;
+		while (letters[j] != '\0')
+		{
+			if (*ptr == letters[j])
+			{
+				*ptr = leets[j];
+				break;
+			}
+			j++;
+		}
+		ptr++;
+	}
+
+	return (str);
+}
