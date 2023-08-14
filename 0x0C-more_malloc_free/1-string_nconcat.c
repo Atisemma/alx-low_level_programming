4929890a1497ba1062f25758253e57f41a5e2b31
Atisemma <atisemma26@gmail.com>
2023-08-14 08:36:23 -0700

    Concatenated strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1 = 0, len2 = 0, i;
+	unsigned int len1 = 0, len2 = 0;
 	char *concat;
 
 	if (s1 == NULL)
@@ -19,22 +19,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[len1] != '\0')
-		len1++;
-	while (s2[len2] != '\0')
-		len2++;
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 
 	if (n >= len2)
 		n = len2;
 
 	concat = malloc(sizeof(char) * (len1 + n + 1));
 	if (concat == NULL)
-		return (NULL);
+		exit(EXIT_FAILURE);
 
-	for (i = 0; i < len1; i++)
-		concat[i] = s1[i];
-	for (i = 0; i < len2; i++)
-		concat[len1 + len2] = '\0';
+	strcpy(concat, s1);
+
+	strncat(concat, s2, n);
 
 	return (concat);
 }
