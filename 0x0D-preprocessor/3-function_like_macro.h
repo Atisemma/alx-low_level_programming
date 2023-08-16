9934163b5154c4181446b1e39feefec3ef917144
Atisemma <atisemma26@gmail.com>
2023-08-15 19:51:41 -0700

    Macro function

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,22 +1,7 @@
-#ifndef ABS_MACRO_H
-#define ABS_MACRO_H
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-#include <stdio.h>
+#define ABS(x) ((x < 0) ? -(x) : (x))
 
-#define ABS(x) ((x) < 0 ? -(x) : (x))
+#endif /* FUNCTION_LIKE_MACRO_H */
 
-/**
- *  main - Entry point
- *  Return: 0
- */
-int main(void)
-{
-	int num1 = -5;
-	int num2 = 10;
-
-	printf("The absolute value of %d is %d\n", num1, ABS(num1));
-	printf("The absolute value of %d is %d\n", num2, ABS(num2));
-
-	return (0);
-}
-#endif
