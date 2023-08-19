f1d7fd58dc97a9459684fd193e5f805cb0f268f9
Atisemma <atisemma26@gmail.com>
2023-08-18 17:21:04 -0700

    Integers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,25 +13,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL || size <= 0)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (array && cmp && size > 0)
 	{
-		if (cmp(array[i] != 0))
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i] != 0))
 			return (i);
+		}
 	}
 
 	return (-1);
-}
-
-/**
- * cmp_function - compare function used to search for the integer 2
- * @value: input to be compared
- *
- * Return: 0 if the value is not 2, othersise non-zero
- */
-int cmp_function(int value)
-{
-	return (value != 2);
 }
