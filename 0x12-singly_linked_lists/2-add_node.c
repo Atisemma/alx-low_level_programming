89d554204d6c3eba7a4fff425381363079f82780
Atisemma <atisemma26@gmail.com>
2023-08-26 18:43:18 -0700

    Added node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head:  pointer to the head of the list
+ * @str: the string to be duplicated and added to the list
+ * Return: theaddress of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	char *duplicate_str;
+	int length = 0;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	duplicate_str = strdup(str);
+	if (duplicate_str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	while (str[length])
+		length++;
+
+	new_node->str = duplicate_str;
+	new_node->len = length;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
