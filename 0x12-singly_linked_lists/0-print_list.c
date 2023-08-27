463e091bb5d88995ad48551576b1696aa7c1e46d
Atisemma <atisemma26@gmail.com>
2023-08-26 18:08:52 -0700

    List elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,6 @@
 /**
  * print_list -prints all the elements of a linked list
  * @h: a pointer to the head of the list
- * @count: number of nodes in the list
  * Return: the number of nodes in the list
  */
 size_t print_list(const list_t *h)
