b4d41ed45d52be2a70cb3f5dfa37d91387277f2f
Atisemma <atisemma26@gmail.com>
2023-09-08 17:46:13 -0700

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,15 +13,20 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-
-	for (i = 0; text_content[i] != '\0'; i++)
-		;
-
-	written_bytes = write(fd, text_content, i);
-	if (written_bytes == -1)
-	{
-		close(fd);
+	if (fd == 1)
 		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+
+		written_bytes = write(fd, text_content, i);
+		if (written_bytes == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
 	close(fd);
