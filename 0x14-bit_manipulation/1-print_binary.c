96e35eb07107adf4c7d150089db2f4a12fac8cb4
Atisemma <atisemma26@gmail.com>
2023-09-17 08:55:52 -0700

    Print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,15 +7,9 @@
  */
 void print_binary(unsigned long int n)
 {
-	int bit = (sizeof(n) * 8) - 1;
+	if (n >> 1)
+		print_binary(n >> 1);
 
-	while (bit >= 0)
-	{
-		if ((n >> bit) & 1)
-			_putchar('1');
-		else
-			_putchar('0');
-		bit--;
-	}
+	_putchar((n & 1) + '0');
 }
 
